f3ab3fa1ffd581ecbbd861abf6d50b359effd5f9
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-05-29 13:37:52 +0200

    chore(concept-client): add testing

---
+++ concept-client/src/components/Hello.test.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Hello } from './Hello';
+
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+    shallow(<Hello />);
+  });
+});
--- concept-client/src/components/Hello.tsx
+++ concept-client/src/components/Hello.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 import './Hello.scss';
 
-export function Hello(props) {
-  return (
-    <div className="hello">
-      <h1>Begrepskatalogen</h1>
-      <h2 className="hellosmall">
-        This the Begrepskatalogen {props.framework} application using {props.compiler} with {props.bundler}
-      </h2>
+export const Hello = props => (
+  <div className="hello">
+    <h1>Begrepskatalogen</h1>
+    <h2 className="hellosmall">
+      This the Begrepskatalogen {props.framework} application using {props.compiler} with {props.bundler}
+    </h2>
 
-      <button className="hellobutton">Click me!</button>
-    </div>
-  );
-}
+    <button className="hellobutton">Click me!</button>
+  </div>
+);
