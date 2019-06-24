34d7e68bd642a6df1ba4a47b6639f1d4ca2ea4ac
Thomas Nøkleby <thomas.nokleby@gmail.com>
2019-06-24 09:35:56 +0200

    Feature: Now creates a new Begrep on POST

--- applications/concept-catalogue/src/main/kotlin/no/begrepskatalog/conceptregistration/BegreperApiImplK.kt
+++ applications/concept-catalogue/src/main/kotlin/no/begrepskatalog/conceptregistration/BegreperApiImplK.kt
@@ -1,5 +1,6 @@
 package no.begrepskatalog.conceptregistration
 
+import io.swagger.annotations.ApiParam
 import no.begrepskatalog.conceptregistration.storage.SqlStore
 import no.begrepskatalog.generated.api.BegreperApi
 import no.begrepskatalog.generated.model.Begrep
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 import javax.servlet.http.HttpServletRequest
+import javax.validation.Valid
 
 
 @RestController
@@ -28,5 +32,16 @@ class BegreperApiImplK : BegreperApi {
 
         val result: MutableList<Begrep> = sqlStore.getBegrepByCompany(orgNumber = placeholderOrgnumber)
         return ResponseEntity.ok(result)
+    }
+
+    override fun createBegrep(httpServletRequest: HttpServletRequest, @ApiParam(value = "", required = true) @Valid @RequestBody begrep: Begrep): ResponseEntity<Void> {
+        logger.info("Create Begrep called with begrep id  ${begrep.id}")
+        var successfulStore = sqlStore.saveBegrep(begrep)
+        if (successfulStore) {
+            logger.info("Stored begrep ${begrep.id}")
+        } else {
+            logger.info("Failed to store begrep. Reason should be in another log line.")
+        }
+        return ResponseEntity.of(Optional.empty())
     }
 }
