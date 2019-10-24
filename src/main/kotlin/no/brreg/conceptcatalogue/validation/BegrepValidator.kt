c73be25366419237161af995b29f0480bd21da05
Bjørn Grøva <bjorn.grova@brreg.no>
2019-10-24 12:43:05 +0200

    feat: Remove validation of organization number control digit, as it has not been specified as a requirement

--- src/main/kotlin/no/brreg/conceptcatalogue/validation/BegrepValidator.kt
+++ src/main/kotlin/no/brreg/conceptcatalogue/validation/BegrepValidator.kt
@@ -24,18 +24,7 @@ fun isValidBegrep(begrep: Begrep): Boolean = when {
 private fun Virksomhet.isValid(): Boolean = when {
     id.isNullOrBlank() -> false
     id.length != 9 -> false
-    !isValidOrganisationNumber(id) -> false
     else -> true
-}
-
-private fun isValidOrganisationNumber(organisationNumber: String): Boolean {
-    val organisationNumberWithoutControlDigit: String = organisationNumber.dropLast(1)
-    val organisationNumberDigits: List<Int> = organisationNumberWithoutControlDigit
-            .split("")
-            .filter { !it.isBlank() }
-            .map { it.toInt() }
-    val controlDigit: Int = ModUtil.calculateMod11Check(organisationNumberDigits, 7)
-    return organisationNumber == "$organisationNumberWithoutControlDigit$controlDigit"
 }
 
 private fun isValidListOfTranslations(translations: Map<String, String>): Boolean = when {
