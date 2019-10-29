bacbadb5a8289ca8deeb02ff807674c53468ed0b
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-10-29 12:32:31 +0100

    fix(concept-catalogue): #159 catch rabbitmq exceptions to avoid unexpected errors

--- src/main/kotlin/no/brreg/conceptcatalogue/service/ConceptPublisher.java
+++ src/main/kotlin/no/brreg/conceptcatalogue/service/ConceptPublisher.java
@@ -2,6 +2,9 @@ package no.brreg.conceptcatalogue.service;
 
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,20 @@ public class ConceptPublisher {
 
     private final AmqpTemplate rabbitTemplate;
 
+    private final static Logger logger = LoggerFactory.getLogger(ConceptPublisher.class);
+
     public ConceptPublisher(AmqpTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     public void send(String publisherId) {
-        ObjectNode payload = JsonNodeFactory.instance.objectNode();
-        payload.put("publisherId", publisherId);
-        rabbitTemplate.convertAndSend(payload);
+        try {
+            ObjectNode payload = JsonNodeFactory.instance.objectNode();
+            payload.put("publisherId", publisherId);
+            rabbitTemplate.convertAndSend(payload);
+            logger.info("Successfully sent harvest message for publisher {}", publisherId);
+        } catch (AmqpException e) {
+            logger.error("Failed to send harvest message for publisher {}", publisherId, e);
+        }
     }
 }
