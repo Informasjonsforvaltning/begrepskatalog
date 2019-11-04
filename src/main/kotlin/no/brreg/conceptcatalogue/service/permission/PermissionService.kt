f4982d07a133c56aaf96a2e7cc97d42e837115eb
Markko Paas <markko.paas@gmail.com>
2019-11-04 15:57:02 +0100

    refactor: rename .security.FdkPermissions to .service.permission.PermissionsService to be better aligned to implementation in the fdk project

--- src/main/kotlin/no/brreg/conceptcatalogue/service/permission/PermissionService.kt
+++ src/main/kotlin/no/brreg/conceptcatalogue/service/permission/PermissionService.kt
@@ -1,12 +1,12 @@
-package no.brreg.conceptcatalogue.security;
+package no.brreg.conceptcatalogue.service.permission
 
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
-@Component
-class FdkPermissions{
+@Service
+class PermissionService {
     private fun authentication(): Authentication {
         return SecurityContextHolder.getContext().authentication
     }
